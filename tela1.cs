ec490215d4526708784b06f43019433441a1e9c3
Pedro Neres <115440600+NEREZZ@users.noreply.github.com>
2024-07-27 11:28:47 -0300

    Update tela1.cs

--- tela1.cs
+++ tela1.cs
@@ -21,8 +21,11 @@ namespace ProjetoLogin
 
         private void tela1_Load(object sender, EventArgs e)
         {
+        // define onde que a tela irá iniciar
             painel.Location = new Point(this.Width / 2 - 218, this.Height / 2 - 198);
+            // define a cor do botao de passar o mouse em cima
             Acessar.FlatAppearance.MouseOverBackColor = Color.FromArgb(252, 239, 164);
+            // define a cor do botao de clicado
             Acessar.FlatAppearance.MouseDownBackColor = Color.FromArgb(216, 200, 119);
         }
 
@@ -37,23 +40,23 @@ namespace ProjetoLogin
             string matricula = caixaUser.Text;
             string senha = caixaSenha.Text;
 
-            // Substitua "sua_cadeia_conexao" pela string de conexão real do seu banco de dados.
+            // string de conexão do banco de dados( chave do banco de dados/endereço).
             string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\pedro\\source\\repos\\ProjetoLogin\\ProjetoLogin\\Usuarios.mdf;Integrated Security=True";
-
+            // abre a conexão com o banco de dados
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
 
-                // Substitua "sua_tabela" pelo nome real da sua tabela no banco de dados.
+                // recebe os valores digitados pelo usuario e adiciona no banco de dados
                 string query = "INSERT INTO informacaoUsuario (matricula, senha) VALUES (@Matricula, @Senha)";
-
+                // inicia o comando para adicionar as informações do usuario e adiciona matricula e senha 
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Matricula", matricula);
                     command.Parameters.AddWithValue("@Senha", senha);
-
+                // verifica se alguma linha foi afetada após enviar os dados
                     int rowsAffected = command.ExecuteNonQuery();
-
+                // se alguma linha for afetada for maior que 0 ou seja modificada/adicionada, deve ser bem sucedido
                     if (rowsAffected > 0)
                     {
                         Console.WriteLine("Dados armazenados no banco de dados com sucesso.");
